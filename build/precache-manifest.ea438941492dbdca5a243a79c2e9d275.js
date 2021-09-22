3427dbcd4b7ea33549c3860df868378611810510
Ted - TedfordMedia <ted@tedfordmedia.com>
2021-09-22 09:05:03 +0100

    initial

---
+++ build/precache-manifest.ea438941492dbdca5a243a79c2e9d275.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad1fab161f5af57b753",
+    "url": "/static/css/main.56d2705d.chunk.css"
+  },
+  {
+    "revision": "cad1fab161f5af57b753",
+    "url": "/static/js/main.cad1fab1.chunk.js"
+  },
+  {
+    "revision": "eade0293b75193285ff9",
+    "url": "/static/js/1.eade0293.chunk.js"
+  },
+  {
+    "revision": "64da8277f43d0711611b",
+    "url": "/static/js/2.64da8277.chunk.js"
+  },
+  {
+    "revision": "9a4c1c25d5f387ae801d",
+    "url": "/static/js/runtime~main.9a4c1c25.js"
+  },
+  {
+    "revision": "6df27f60b3218939910f360a09eb12e4",
+    "url": "/static/media/showContentInfo.6df27f60.mp3"
+  },
+  {
+    "revision": "daa0c251fabd55cfb80bf342595f7f86",
+    "url": "/static/media/enterGame.daa0c251.mp3"
+  },
+  {
+    "revision": "b640f9b53555780fb4f5288095c5722d",
+    "url": "/static/media/open.b640f9b5.mp3"
+  },
+  {
+    "revision": "fd3f95b87091ad7191dff4a8a8785184",
+    "url": "/static/media/bidi.fd3f95b8.mjs"
+  },
+  {
+    "revision": "a045c5b818b9a5dbd1c0da85a9209292",
+    "url": "/static/media/index.a045c5b8.mjs"
+  },
+  {
+    "revision": "fb37267e5791f3fff94793641eef9d74",
+    "url": "/index.html"
+  }
+];
